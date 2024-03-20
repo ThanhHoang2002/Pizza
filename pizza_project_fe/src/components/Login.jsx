cbb241bcd03b1449d3d2b97169e7c52797bcf74c
ThanhHoang2002 <thanhhoang11102002@gmail.com>
2024-03-20 19:13:20 +0700

    finish Sign Up

--- pizza_project_fe/src/components/Login.jsx
+++ pizza_project_fe/src/components/Login.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { update } from '../redux/slices/loginSlice';
 
 const Login = () => {
-  const [hidePassword, setHidePassword] = useState(false)
+  const [hiddenPassword, setHiddenPassword] = useState(true)
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
+  const navigate = useNavigate()
   const dispatch = useDispatch()
   const handleCloseMember =()=>{
     dispatch(update())
+  }
+  const handleChoseSignUp  =()=>{
+    dispatch(update())
+    navigate('/sign-up')
   }
   return (
     <div className='relative'>
@@ -29,12 +34,12 @@ const Login = () => {
               </p>
             <label className='font-bold text-sm'> Mật Khẩu *</label>
             <div className='w-full flex items-center'>
-              <input className=' w-[90%] h-[40px]  px-[14px] py-[10.5px]  border-2 rounded-[4px] border-[rgba(70, 90, 126, 0.4)] focus:outline-none focus:border-[#07bc0c]' type={`${hidePassword===true ?'text':'password'}`} value={password} onChange={(e) => setPassword(e.target.value)}></input>
+              <input className=' w-[90%] h-[40px]  px-[14px] py-[10.5px]  border-2 rounded-[4px] border-[rgba(70, 90, 126, 0.4)] focus:outline-none focus:border-[#07bc0c]' type={`${hiddenPassword===true ?'text':'password'}`} value={password} onChange={(e) => setPassword(e.target.value)}></input>
               {
-                hidePassword === true ?
-                <img className='ml-2 p-[8px] cursor-pointer h-[40px] w-[40px] rounded-[50%] hover:bg-slate-100 hover:transition-all' src='https://firebasestorage.googleapis.com/v0/b/pizza-fe093.appspot.com/o/image%2Ficon%2Ficons8-eye-30.png?alt=media&token=b5825d15-562b-43b6-8c33-b937a20a5ba6' alt='eye' onClick={() => setHidePassword(false)}></img>
+                hiddenPassword === false ?
+                <img className='ml-2 p-[8px] cursor-pointer h-[40px] w-[40px] rounded-[50%] hover:bg-slate-100 hover:transition-all' src='https://firebasestorage.googleapis.com/v0/b/pizza-fe093.appspot.com/o/image%2Ficon%2Ficons8-eye-30.png?alt=media&token=b5825d15-562b-43b6-8c33-b937a20a5ba6' alt='eye' onClick={() => setHiddenPassword(true)}></img>
                 :
-                <img className='ml-2 p-[8px] cursor-pointer h-[40px] w-[40px] rounded-[50%] hover:bg-slate-100 hover:transition-all' src='https://firebasestorage.googleapis.com/v0/b/pizza-fe093.appspot.com/o/image%2Ficon%2Ficons8-hide-24.png?alt=media&token=bcb6d1a8-da9f-4033-94f4-ecb7d84b64d5' alt='eye' onClick={() => setHidePassword(true)}></img>
+                <img className='ml-2 p-[8px] cursor-pointer h-[40px] w-[40px] rounded-[50%] hover:bg-slate-100 hover:transition-all' src='https://firebasestorage.googleapis.com/v0/b/pizza-fe093.appspot.com/o/image%2Ficon%2Ficons8-hide-24.png?alt=media&token=bcb6d1a8-da9f-4033-94f4-ecb7d84b64d5' alt='eye' onClick={() => setHiddenPassword(false)}></img>
               }
             </div>
             <div className='flex justify-between items-center mt-3'>
@@ -48,10 +53,10 @@ const Login = () => {
             <button className='bg-[#0A8020] mt-5 text-white w-[80%] h-[33px] mx-[10%] rounded-[4px] text-sm'>ĐĂNG NHẬP</button>
           </form>
           <div className='text-sm m-[18px]'>
-            Bạn chưa có tài khoản? 
-            <span>
-              <Link to='/sign-up' className='text-[rgb(10,128,32)]'> Đăng ký ngay</Link>
-            </span>
+            Bạn chưa có tài khoản? <bt/>
+            <button className='text-[rgb(10,128,32)]'onClick={handleChoseSignUp}>
+              Đăng ký ngay
+            </button>
           </div>
         </div>
         <button className='absolute top-[-10px] right-[-15px]' onClick={handleCloseMember}>
